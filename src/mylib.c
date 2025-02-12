e1977fe2c9fcd92b2948ce546f320e32cf242bf7
Antospy-79@hotmail.fr <antospy-79@hotmail.fr>
2025-02-12 12:05:19 +0100

    update: drawSpiral

--- src/mylib.c
+++ src/mylib.c
@@ -3,12 +3,20 @@
 #include "../include/config.h"
 #include "../include/mylib.h"
 
-void drawSpiral(SDL_Renderer *renderer) {
+void drawSpiral(SDL_Renderer *renderer, const char *filename) {
+    FILE *file = fopen (filename, "w");
+    if(file == NULL){
+        perror("erreur d'ouverture du fichier.");
+        return;
+    }
 
     // Spirale bleue
     for (int t = 0; t < WINDOW_WIDTH/sqrt(2); t+=2) {
         int x = WINDOW_WIDTH/2 + t*cos(t*M_PI/180);
         int y = WINDOW_HEIGHT/2 + t*sin(t*M_PI/180);
+        if(x < WINDOW_WIDTH && y < WINDOW_HEIGHT){
+        fprintf(file, "[ x: %d - y: %d ] - bleu\n", x, y);
+        }
         SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE); // Bleu
         SDL_RenderDrawPoint(renderer, x, y);
     }
@@ -17,9 +25,13 @@ void drawSpiral(SDL_Renderer *renderer) {
     for (int t = 1; t < WINDOW_WIDTH/sqrt(2); t+=2) {
         int x = WINDOW_WIDTH/2 - t*cos(t*M_PI/180);
         int y = WINDOW_HEIGHT/2 - t*sin(t*M_PI/180);
+        if(x < WINDOW_WIDTH && y < WINDOW_HEIGHT){
+        fprintf(file, "[ x: %d - y: %d ] - rouge\n", x, y);
+        }
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE); // Rouge
         SDL_RenderDrawPoint(renderer, x, y);
     }
 
+    fclose(file);
     SDL_RenderPresent(renderer);
 }
