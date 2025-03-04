6316330ff16f4f1860a52dbe1da4cff8ae92be92
thebattlecrw <apaduela@gmail.com>
2025-03-04 16:25:23 +0100

    feat: added documentation, documented ForwardPass.h

--- include/neuralnet/ForwardPass.h
+++ include/neuralnet/ForwardPass.h
@@ -3,12 +3,39 @@
 
 #include <SDL2/SDL.h>
 
-//void forwardPass(Layer input_layer);
+/**
+ * @brief Sets the value of a neuron in the layer
+ * @param layer The layer where the neuron will be changed
+ * @param index The index of the neuron in the layer->neurons table where
+ * @param value The value of the neuron
+ */
 void initNeuron(Layer layer, int index, float value);
+
+/**
+ * @brief Initialises the input values with a random number between 0 and 1
+ * @param inputs The initial values of the 
+ */
 void initRandomInputs(float *inputs, int size);
+
+/**
+ * @brief Transfer function used to put a value between 0 and 1
+ * @returns The hyperbolic tangent applied to the parameter x
+ */
 float activationFunction(float x); // fonction de tranfert
+
+/**
+ * @brief Calculates the value of a neuron in the next layer
+ * @param neuronIndex The index of the neuron in the next layer
+ */
 void computeNextNeuronValue(Layer currentLayer, int neuronIndex);
+
 void processNeuronLayer(Layer inputLayer, Layer hiddenLayer, float *inputValues, int inputSize);
+
+/**
+ * @brief Initialises a pre-allocated array of floats of a given size with random numbers
+ * @param array Pointer to the float array
+ * @param size The size of the array
+ */
 void initRandomFloatArray(float *array, int size);
 
 /**
