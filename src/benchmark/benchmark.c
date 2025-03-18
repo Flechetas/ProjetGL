0b97efe42e41e3c8a6bf9074580cec5079edae63
Baubet <xavier.baubet@gmail.com>
2025-03-18 20:50:46 +0100

    Feat: benchmark forward & bac prog

--- src/benchmark/benchmark.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "benchmark/benchmark_ForwardPass.h"
-
-int main() {
-    int iterations = 1000; // Nombre d'exécutions pour le benchmark
-    benchmarkForwardPass(iterations);
-    return 0;
-}
--- src/benchmark/benchmark_BackPropagation.c
+++ src/benchmark/benchmark_BackPropagation.c
@@ -2,26 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include "neuralnet/model.h"
-#include "neuralnet/ForwardPass.h"
-#include "benchmark/benchmark_ForwardPass.h"
+#include "neuralnet/backpropagation.h"
 
-
-void benchmarkForwardPass(int iterations, int nb_layer,int input_size, int hidden_size, int output_size) {
-
-    // Création d'un modèle de réseau de neurones avec des tailles aléatoires
+void benchmarkBackpropagation(int iterations, int nb_layer, int input_size, int hidden_size, int output_size, float training_step) {
+    
+    // Création d'un modèle de réseau de neurones aléatoire
     Model model = createModelRandom(nb_layer, input_size, hidden_size, output_size);
     
-    // Initialisation des valeurs d'entrée
+    // Initialisation des valeurs d'entrée et de sortie attendue
     float input[input_size];
+    float expected[output_size];
     for (int i = 0; i < input_size; i++) {
         input[i] = ((float) rand() / RAND_MAX) * 2.0 - 1.0; // Valeurs entre -1 et 1
+    }
+    for (int i = 0; i < output_size; i++) {
+        expected[i] = ((float) rand() / RAND_MAX) * 2.0 - 1.0;
     }
 
     // Capture du temps avant l'exécution des itérations
     clock_t start = clock();
     
     for (int i = 0; i < iterations; i++) {
-        forwardPass(model, input);
+        backpropagate(model, training_step, input, expected);
     }
     
     // Capture du temps après l'exécution
@@ -29,9 +31,9 @@ void benchmarkForwardPass(int iterations, int nb_layer,int input_size, int hidde
 
     // Calcul du temps total (différence entre le temps de fin et de début en ms) 
     double total_time = ((double)(end - start)) / CLOCKS_PER_SEC * 1000.0;
-    printf("\n=== Benchmark de la propagation (ForwardPass) ===\n");
+    printf("\n=== Benchmark de la rétropropagation (Backpropagation) ===\n");
     printf("Nombre d'itérations : %d\n", iterations);
-    printf("Model utilisé : %d couches (Nb neuronnes -> Entrée: %d, Cachée: %d, Sortie: %d)\n\n", nb_layer, input_size, hidden_size, output_size);
+    printf("Modèle utilisé : %d couches (Entrée: %d, Cachée: %d, Sortie: %d)\n", nb_layer, input_size, hidden_size, output_size);
     printf("Temps total : %.4f ms\n", total_time);
     printf("Temps moyen par itération : %.4f ms\n\n", total_time / iterations);
     
