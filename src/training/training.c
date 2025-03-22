e8f1d64f68dec890b34c8f733ca7b7e11f49f155
Olyxz16 <cedric.sazos@tutanota.com>
2025-03-22 15:13:20 +0100

    refac: change log to display 100th step

--- src/training/training.c
+++ src/training/training.c
@@ -3,6 +3,7 @@
 #include "neuralnet/model.h"
 #include <stdio.h>
 
+#define DISPLAY_STEP 100
 
 int train(Model model, float training_step, int batch_size, float **inputs, int input_size, float **expecteds, int expected_size) {
     if(firstLayer(model)->n != input_size) {
@@ -12,9 +13,15 @@ int train(Model model, float training_step, int batch_size, float **inputs, int 
         return -1;
     }
     
+    float avg_cost = 0;
+    int step = batch_size / DISPLAY_STEP;
     for(int i = 0 ; i < batch_size ; i++) {
         float cost = backpropagate(model, training_step, inputs[i], expecteds[i]);
-        printf("Training step %d | cost : %f\n", i, cost);
+        avg_cost += cost;
+        if(i % step == 0) {
+            printf("Training step %d | cost : %0.10f\n", i, avg_cost / step);
+            avg_cost = 0;
+        }
     }
 
     return 0;
