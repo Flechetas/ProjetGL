d9e20aa743ec28ffefeccf28e7d796db4a16236a
Olyxz16 <cedric.sazos@tutanota.com>
2025-03-23 22:02:41 +0100

    docs: more docs

--- include/cli/cli.h
+++ include/cli/cli.h
@@ -3,15 +3,50 @@
 
 /**
  * @brief parses the flags
- * @
+ * @param argc number of arguments
+ * @param argv arguments
+ * @return 0 on success, error code otherwise
  */
 int exec(int argc, char *argv[]);
 
+/**
+ * @brief parses the draw command
+ * @param argc number of arguments
+ * @param argv arguments
+ * @return 0 on success, error code otherwise
+ */
 int draw(int argc, char *argv[]);
+
+/**
+ * @brief parses the train command
+ * @param argc number of arguments
+ * @param argv arguments
+ * @return 0 on success, error code otherwise
+ */
 int execTrain(int argc, char *argv[]);
+
+/**
+ * @brief parses the generate command
+ * @param argc number of arguments
+ * @param argv arguments
+ * @return 0 on success, error code otherwise
+ */
 int generate(int argc, char *argv[]);
+
+/**
+ * @brief displays help
+ * @param argc number of arguments
+ * @param argv arguments
+ * @return 0 on success, error code otherwise
+ */
 int displayHelp(int argc, char *argv[]);
 
+/*
+ * @brief check if string has suffix suff
+ * @param string source string
+ * @param suff suffix to search in source string
+ * @return 0 on success, error code otherwise
+ */
 int endsWith(char *string, const char *suff);
 
 #endif //CLI_H
