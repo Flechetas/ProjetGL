2dffe18406a8bdbe016cdd1d5515ef9b1a24c5fc
Olyxz16 <cedric.sazos@tutanota.com>
2025-03-26 21:10:38 +0100

    refac: remove unused code

--- include/neuralnet/ForwardPass.h
+++ include/neuralnet/ForwardPass.h
@@ -6,59 +6,10 @@
 #include "model.h"
 
 /**
- * @brief Sets the value of a neuron in the layer
- * @param layer The layer where the neuron will be changed
- * @param index The index of the neuron in the layer->neurons table where
- * @param value The value of the neuron
- */
-void initNeuron(Layer layer, int index, float value);
-
-/**
- * @brief Initialises the input values with a random number between 0 and 1
- * @param inputs The initial values of the 
- */
-void initRandomInputs(float *inputs, int size);
-
-/**
  * @brief Transfer function used to put a value between 0 and 1
  * @returns The hyperbolic tangent applied to the parameter x
  */
 float activationFunction(float x); // fonction de tranfert
-
-/**
- * @brief Calculates the value of a neuron in the next layer
- * @param neuronIndex The index of the neuron in the next layer
- */
-void computeNextNeuronValue(Layer currentLayer, int neuronIndex);
-
-/**
- * @brief Initialise les neurones d'entrée et propage les valeurs vers la première couche cachée.
- * @param inputLayer La couche d'entrée du réseau de neurones.
- * @param hiddenLayer La couche cachée qui recevra les valeurs calculées.
- * @param inputValues Un tableau contenant les valeurs d'entrée.
- * @param inputSize La taille du tableau d'entrée.
- */
-void processNeuronLayer(Layer inputLayer, Layer hiddenLayer, float *inputValues, int inputSize);
-
-/**
- * @brief Initialises a pre-allocated array of floats of a given size with random numbers
- * @param array Pointer to the float array
- * @param size The size of the array
- */
-void initRandomFloatArray(float *array, int size);
-
-/**
- * @brief Initialise les neurones d'une couche avec un tableau de valeurs
- * @param inputLayer La couche d'entrée
- * @param values Les valeurs des neurones
- */
-void initializeLayerNeurons(Layer inputLayer, float *values);
-
-/**
- * @brief Effectue la propagation d'une couche à la suivante
- * @param current La couche actuelle
- */
- void propagateLayer(Layer current);
 
 /**
  * @brief Effectue la propagation sur un réseau de neurones
--- src/neuralnet/ForwardPass.c
+++ src/neuralnet/ForwardPass.c
@@ -7,74 +7,12 @@
 #include "neuralnet/layer.h"
 #include "neuralnet/ForwardPass.h"
 
-
-// Fonction d'initialisation d'un neurone pour un pixel donné 
-void initNeuron(Layer layer, int index, float value) 
-{
-    if (index < layer->n) {
-        layer->neurons[index] = value;
-    }
-}
-
-// initialise les entrées aléatoirement entre -1 et 1
-void initRandomInputs(float *inputs, int size) {
-    for (int i = 0; i < size; i++) {
-        inputs[i] = ((float)rand() / RAND_MAX) * 2.0 - 1.0;
-    }
-}
-
 // Fonction de transfert (tangente hyperbolique)
 float activationFunction(float x)
 {
     return tanhf(x);
 }
 
-
-// Fonction qui calcule la valeur du neurone suivant dans la couche cachée
-void computeNextNeuronValue(Layer currentLayer, int neuronIndex) {
-    if (currentLayer->next == NULL) {
-        printf("Erreur: la couche suivante n'existe pas.\n");
-        return;
-    }
-    
-    float sum = 0.0;
-    for (int i = 0; i < currentLayer->n; i++) {
-        sum += currentLayer->neurons[i] * currentLayer->weight[i][neuronIndex];
-    }
-    
-    currentLayer->next->neurons[neuronIndex] = activationFunction(sum);
-}
-
-// Fonction qui initialise les neurones et calcule les valeurs pour la couche cachée
-void processNeuronLayer(Layer inputLayer, Layer hiddenLayer, float *inputValues, int inputSize) {
-    if (inputSize != inputLayer->n) {
-        printf("Erreur: La taille des entrées ne correspond pas au nombre de neurones de la couche d'entrée.\n");
-        return;
-    }
-    
-    // Initialisation des neurones de la couche d'entrée
-    for (int i = 0; i < inputSize; i++) {
-        initNeuron(inputLayer, i, inputValues[i]);
-    }
-    
-    // Calcul des valeurs des neurones de la couche cachée
-    for (int i = 0; i < hiddenLayer->n; i++) {
-        computeNextNeuronValue(inputLayer, i);
-    }
-}
-
-void initRandomFloatArray(float *array, int size) {
-    for (int i = 0; i < size; i++) {
-        array[i] = ((float)rand() / RAND_MAX) * 2.0 - 1.0;
-    }
-}
-
-/**
- * @brief Effectue la propagation sur un réseau de neurones
- * @param model Le model du réseau de neurones
- * @param input Le vecteur d'entrée (valeur des neuronnes d'entrés) // random init ?
- * @return Les valeurs des neurones de la couche de sortie
- */
 float* forwardPass(Model model, float *input) {
     // Affectation des valeurs d'entrée à la couche d'entrée
     Layer current = model->input;
