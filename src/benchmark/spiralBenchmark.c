b5883b636c49c3346db4d1f780b2a2688d4cb8df
thebattlecrw <apaduela@gmail.com>
2025-03-28 21:52:06 +0100

    fix: removed unnecessary benchmarks

--- src/benchmark/spiralBenchmark.c
+++ src/benchmark/spiralBenchmark.c
@@ -35,37 +35,6 @@ float* getOutputValues(Model model) {
     return lastLayer(model)->neurons;
 }
 
-void benchmarkAccuracyBRUT(Model model, float **inputs, float **expected_outputs, int nb_samples, int output_size) {
-    int correct = 0;
-
-    clock_t start = clock();
-    for (int i = 0; i < nb_samples; i++) {
-        //float *prediction = forwardPass(model, inputs[i]);
-        float *prediction = getOutputValues(model);
-        int predicted_index = getMaxIndex(prediction, output_size);
-        int expected_index = getMaxIndex(expected_outputs[i], output_size);
-
-        if (predicted_index == expected_index) {
-            correct++;
-        }
-    }
-
-
-    clock_t end = clock();
-
-    double total_time = (double)(end - start) / CLOCKS_PER_SEC;
-    double avg_time = total_time / nb_samples * 1000.0;
-    int minutes = (int)(total_time / 60);
-    double seconds = total_time - minutes * 60;
-    float accuracy = (float)correct / nb_samples;
-    printf("=== Résultat du benchmark (précision expected_outputs) ===\n");
-    printf("Corrects : %d / %d (itérations)\n", correct, nb_samples);
-    printf("Précision : %.2f%%\n", accuracy * 100.0);
-    printf("Temps total : %d min %.2f sec\n", minutes, seconds);
-    printf("Temps moyen par instance : %.4f ms\n", avg_time);
-
-}
-
 void benchmarkAccuracyPoints(Model model, int output_size){
     
     //initSpiralPoints(); 
@@ -77,8 +46,10 @@ void benchmarkAccuracyPoints(Model model, int output_size){
     Point *blue_points = getBluePoints();
     int red_count = getRlen();
     int blue_count = getBlen();
-    printf("red_count = %d\n", red_count);
-    printf("blue_count = %d\n", blue_count);
+
+    log_debug("red_count = %d\n", red_count);
+    log_debug("blue_count = %d\n", blue_count);
+
     int correctSpi = 0;
     int total = red_count + blue_count;
 
@@ -162,13 +133,10 @@ void benchmarkAccuracyDetermineColor(Model model, float **inputs, int nb_samples
     printf("Temps moyen par instance : %.4f ms\n", avg_time);
 }
 
-void benchmarkAccuracyAndTime(Model model, float **inputs, int nb_samples, float **expected_outputs, int output_size, clock_t start, clock_t end) {
+void benchmarkAccuracyAndTime(Model model, float **inputs, int nb_samples, int output_size) {
 
     // Accuracy par points 
     benchmarkAccuracyPoints(model, output_size);
-
-    // Accuracy tab expected
-    benchmarkAccuracyBRUT(model, inputs, expected_outputs, nb_samples, output_size);
 
     // Accuracy avec DetermineColor()
     benchmarkAccuracyDetermineColor(model, inputs, nb_samples, output_size);
@@ -186,41 +154,25 @@ int benchmarkSpiral(Model model) {
 
     log_trace("Precomputing training values...\n");
     srand(time(NULL));
-    int r;
-    int b;
+   
     int xMax = WINDOW_WIDTH;
     int yMax = WINDOW_HEIGHT;
     float **inputs = malloc(SAMPLE_SIZE * sizeof(float *));
-    float **expecteds = malloc(SAMPLE_SIZE * sizeof(float *));
     for(int i = 0 ; i < SAMPLE_SIZE ; i++) {
         float x = rand() % xMax;
         float y = rand() % yMax;
         inputs[i] = malloc(2 * sizeof(float));
         inputs[i][0] = x / (float)xMax;
         inputs[i][1] = y / (float)yMax;
-        
-        determineColor(x, y, &r, &b);
-        expecteds[i] = malloc(2 * sizeof(float));
-        if(r > b) {
-            expecteds[i][0] = 1.0;
-            expecteds[i][1] = 0.0;
-        } else {
-            expecteds[i][0] = 0.0;
-            expecteds[i][1] = 1.0;
-        }
     }
     log_trace("Precomputing complete.\n");
 
-    clock_t start = clock();
-    clock_t end = clock();
-    benchmarkAccuracyAndTime(model, inputs, SAMPLE_SIZE, expecteds, 2, start, end);
+    benchmarkAccuracyAndTime(model, inputs, SAMPLE_SIZE, 2);
 
     for(int i = 0 ; i < SAMPLE_SIZE ; i++) {
         free(inputs[i]);
-        free(expecteds[i]);
     }
     free(inputs);
-    free(expecteds);
     freePoints();
 
     return 0;
